ab52795487a08ccc08ebd053c1c0710b85aa6afc
Joe Arrowood <jarrow@ctvea.net>
2022-01-17 07:38:28 -0500

    Implementing SqlClient

---
+++ JWSysWrapper/Impl/Data/DataTableWrap.cs
@@ -0,0 +1,137 @@
+﻿#region © 2022 JoeWare.
+//
+// All rights reserved. Reproduction or transmission in whole or in part, in
+// any form or by any means, electronic, mechanical, or otherwise, is prohibited
+// without the prior written consent of the copyright owner.
+//
+#endregion
+
+using System;
+using System.IO;
+using System.Xml;
+using System.Data;
+using System.Globalization;
+using System.ComponentModel;
+using System.Runtime.Serialization;
+
+using JWSysWrap.Interface.Data;
+
+namespace JWSysWrap.Impl.Data
+{
+    // ----------------------------------------------------
+    /// <summary>
+    ///     DataTableWrap Description
+    /// </summary>
+
+    public class DataTableWrap : IDataTable
+    {
+        public DataTable Instance { get; private set; }
+
+        // ------------------------------------------------
+
+        public DataTableWrap() => Initialize();
+        public DataTableWrap(string tableName) => Initialize(tableName);
+        public DataTableWrap(DataTable dataTable) => Initialize(dataTable);
+        public DataTableWrap(string tableName, string tableNamespace) => Initialize(tableName, tableNamespace);
+
+        // ------------------------------------------------
+
+        public event DataColumnChangeEventHandler ColumnChanged { add => Instance.ColumnChanged += value; remove => Instance.ColumnChanged -= value; }
+        public event DataColumnChangeEventHandler ColumnChanging { add => Instance.ColumnChanging += value; remove => Instance.ColumnChanging -= value; }
+        public event EventHandler Initialized { add => Instance.Initialized += value; remove => Instance.Initialized -= value; }
+        public event DataRowChangeEventHandler RowChanged { add => Instance.RowChanged += value; remove => Instance.RowChanged -= value; }
+        public event DataRowChangeEventHandler RowChanging { add => Instance.RowChanging += value; remove => Instance.RowChanging -= value; }
+        public event DataRowChangeEventHandler RowDeleted { add => Instance.RowDeleted += value; remove => Instance.RowDeleted -= value; }
+        public event DataRowChangeEventHandler RowDeleting { add => Instance.RowDeleting += value; remove => Instance.RowDeleting -= value; }
+        public event DataTableClearEventHandler TableCleared { add => Instance.TableCleared += value;  remove => Instance.TableCleared -= value; }
+        public event DataTableClearEventHandler TableClearing { add => Instance.TableClearing += value;  remove => Instance.TableClearing -= value; }
+        public event DataTableNewRowEventHandler TableNewRow { add => Instance.TableNewRow += value;  remove => Instance.TableNewRow -= value; }
+
+        // ------------------------------------------------
+
+        public bool CaseSensitive { get => Instance.CaseSensitive;  set => Instance.CaseSensitive = value; }
+        public DataRelationCollection ChildRelations { get => Instance.ChildRelations; }
+        public DataColumnCollection Columns { get => Instance.Columns; }
+        public ConstraintCollection Constraints { get => Instance.Constraints; }
+        public DataSet DataSet { get => Instance.DataSet; }
+        public DataView DefaultView { get => Instance.DefaultView; }
+        public string DisplayExpression { get => Instance.DisplayExpression;  set => Instance.DisplayExpression = value; }
+        public PropertyCollection ExtendedProperties { get => Instance.ExtendedProperties; }
+        public bool HasErrors { get => Instance.HasErrors; }
+        public bool IsInitialized { get => Instance.IsInitialized; }
+        public CultureInfo Locale { get => Instance.Locale;  set => Instance.Locale = value; }
+        public int MinimumCapacity { get => Instance.MinimumCapacity;  set => Instance.MinimumCapacity = value; }
+        public string Namespace { get => Instance.Namespace;  set => Instance.Namespace = value; }
+        public DataRelationCollection ParentRelations { get => Instance.ParentRelations; }
+        public string Prefix { get => Instance.Prefix;  set => Instance.Prefix = value; }
+        public DataColumn[] PrimaryKey { get => Instance.PrimaryKey;  set => Instance.PrimaryKey = value; }
+        public SerializationFormat RemotingFormat { get => Instance.RemotingFormat;  set => Instance.RemotingFormat = value; }
+        public DataRowCollection Rows { get => Instance.Rows; }
+        public ISite Site { get => Instance.Site;  set => Instance.Site = value; }
+        public string TableName { get => Instance.TableName;  set => Instance.TableName = value; }
+        public void AcceptChanges() => Instance.AcceptChanges();
+        public void BeginInit() => Instance.BeginInit();
+        public void BeginLoadData() => Instance.BeginLoadData();
+        public void Clear() => Instance.Clear();
+        public IDataTable Clone() => new DataTableWrap(Instance.Clone());
+        public object Compute(string expression, string filter) => Instance.Compute(expression, filter);
+        public IDataTable Copy() => new DataTableWrap(Instance.Copy());
+        public DataTableReader CreateDataReader() => Instance.CreateDataReader();
+        public void EndInit() => Instance.EndInit();
+        public void EndLoadData() => Instance.EndLoadData();
+        public IDataTable GetChanges() => new DataTableWrap(Instance.GetChanges());
+        public IDataTable GetChanges(DataRowState rowStates) => new DataTableWrap(Instance.GetChanges(rowStates));
+        public DataRow[] GetErrors() => Instance.GetErrors();
+        public void GetObjectData(SerializationInfo info, StreamingContext context) => Instance.GetObjectData(info, context);
+        public void ImportRow(DataRow row) => Instance.ImportRow(row);
+        public void Load(IDataReader reader) => Instance.Load(reader);
+        public void Load(IDataReader reader, LoadOption loadOption) => Instance.Load(reader, loadOption);
+        public void Load(IDataReader reader, LoadOption loadOption, FillErrorEventHandler errorHandler) => Instance.Load(reader, loadOption, errorHandler);
+        public DataRow LoadDataRow(object[] values, bool acceptChanges) => Instance.LoadDataRow(values, acceptChanges);
+        public DataRow LoadDataRow(object[] values, LoadOption loadOption) => Instance.LoadDataRow(values, loadOption);
+        public void Merge(DataTable table) => Instance.Merge(table);
+        public void Merge(DataTable table, bool preserveChanges) => Instance.Merge(table, preserveChanges);
+        public void Merge(DataTable table, bool preserveChanges, MissingSchemaAction missingSchemaAction) => 
+            Instance.Merge(table, preserveChanges, missingSchemaAction);
+        public DataRow NewRow() => Instance.NewRow();
+        public XmlReadMode ReadXml(Stream stream) => Instance.ReadXml(stream);
+        public XmlReadMode ReadXml(TextReader reader) => Instance.ReadXml(reader);
+        public XmlReadMode ReadXml(string fileName) => Instance.ReadXml(fileName);
+        public XmlReadMode ReadXml(XmlReader reader) => Instance.ReadXml(reader);
+        public void ReadXmlSchema(Stream stream) => Instance.ReadXmlSchema(stream);
+        public void ReadXmlSchema(TextReader reader) => Instance.ReadXmlSchema(reader);
+        public void ReadXmlSchema(string fileName) => Instance.ReadXmlSchema(fileName);
+        public void ReadXmlSchema(XmlReader reader) => Instance.ReadXmlSchema(reader);
+        public void RejectChanges() => Instance.RejectChanges();
+        public void Reset() => Instance.Reset();
+        public DataRow[] Select() => Instance.Select();
+        public DataRow[] Select(string filterExpression) => Instance.Select(filterExpression);
+        public DataRow[] Select(string filterExpression, string sort) => Instance.Select(filterExpression, sort);
+        public DataRow[] Select(string filterExpression, string sort, DataViewRowState recordStates) => Instance.Select(filterExpression, sort, recordStates);
+        public void WriteXml(Stream stream) => Instance.WriteXml(stream);
+        public void WriteXml(Stream stream, bool writeHierarchy) => Instance.WriteXml(stream, writeHierarchy);
+        public void WriteXml(TextWriter writer) => Instance.WriteXml(writer);
+        public void WriteXml(TextWriter writer, bool writeHierarchy) => Instance.WriteXml(writer, writeHierarchy);
+        public void WriteXml(XmlWriter writer) => Instance.WriteXml(writer);
+        public void WriteXml(XmlWriter writer, bool writeHierarchy) => Instance.WriteXml(writer, writeHierarchy);
+        public void WriteXml(string fileName) => Instance.WriteXml(fileName);
+        public void WriteXml(string fileName, bool writeHierarchy) => Instance.WriteXml(fileName, writeHierarchy);
+        public void WriteXml(Stream stream, XmlWriteMode mode) => Instance.WriteXml(stream, mode);
+        public void WriteXml(Stream stream, XmlWriteMode mode, bool writeHierarchy) => Instance.WriteXml(stream, mode, writeHierarchy);
+        public void WriteXml(TextWriter writer, XmlWriteMode mode) => Instance.WriteXml(writer, mode);
+        public void WriteXml(TextWriter writer, XmlWriteMode mode, bool writeHierarchy) => Instance.WriteXml(writer, mode, writeHierarchy);
+        public void WriteXml(XmlWriter writer, XmlWriteMode mode) => Instance.WriteXml(writer, mode);
+        public void WriteXml(XmlWriter writer, XmlWriteMode mode, bool writeHierarchy) => Instance.WriteXml(writer, mode, writeHierarchy);
+        public void WriteXml(string fileName, XmlWriteMode mode) => Instance.WriteXml(fileName, mode);
+        public void WriteXml(string fileName, XmlWriteMode mode, bool writeHierarchy) => Instance.WriteXml(fileName, mode, writeHierarchy);
+        public void WriteXmlSchema(Stream stream) => Instance.WriteXmlSchema(stream);
+        public void WriteXmlSchema(Stream stream, bool writeHierarchy) => Instance.WriteXmlSchema(stream, writeHierarchy);
+        public void WriteXmlSchema(TextWriter writer) => Instance.WriteXmlSchema(writer);
+        public void WriteXmlSchema(TextWriter writer, bool writeHierarchy) => Instance.WriteXmlSchema(writer, writeHierarchy);
+        private void Initialize() => Instance = new DataTable();
+        private void Initialize(string tableName) => Instance = new DataTable(tableName);
+        private void Initialize(string tableName, string tableNamespace) => Instance = new DataTable(tableName, tableNamespace);
+        private void Initialize(DataTable dataTable) => Instance = dataTable;
+        public void Dispose() => Instance.Dispose();
+    }
+}
---
+++ JWSysWrapper/Interface/Data/IDataTable.cs
@@ -0,0 +1,113 @@
+﻿#region © 2022 JoeWare.
+//
+// All rights reserved. Reproduction or transmission in whole or in part, in
+// any form or by any means, electronic, mechanical, or otherwise, is prohibited
+// without the prior written consent of the copyright owner.
+//
+#endregion
+
+using System;
+using System.IO;
+using System.Xml;
+using System.Data;
+using System.Globalization;
+using System.ComponentModel;
+using System.Runtime.Serialization;
+
+namespace JWSysWrap.Interface.Data
+{
+    public interface IDataTable : IDisposable
+    {
+        DataTable Instance { get; }
+
+        event DataColumnChangeEventHandler ColumnChanged;
+        event DataColumnChangeEventHandler ColumnChanging;
+        event EventHandler Initialized;
+        event DataRowChangeEventHandler RowChanged;
+        event DataRowChangeEventHandler RowChanging;
+        event DataRowChangeEventHandler RowDeleted;
+        event DataRowChangeEventHandler RowDeleting;
+        event DataTableClearEventHandler TableCleared;
+        event DataTableClearEventHandler TableClearing;
+        event DataTableNewRowEventHandler TableNewRow;
+        bool CaseSensitive { get; set; }
+        DataRelationCollection ChildRelations { get; }
+        DataColumnCollection Columns { get; }
+        ConstraintCollection Constraints { get; }
+        DataSet DataSet { get; }
+        DataView DefaultView { get; }
+        string DisplayExpression { get; set; }
+        PropertyCollection ExtendedProperties { get; }
+        bool HasErrors { get; }
+        bool IsInitialized { get; }
+        CultureInfo Locale { get; set; }
+        int MinimumCapacity { get; set; }
+        string Namespace { get; set; }
+        DataRelationCollection ParentRelations { get; }
+        string Prefix { get; set; }
+        DataColumn[] PrimaryKey { get; set; }
+        SerializationFormat RemotingFormat { get; set; }
+        DataRowCollection Rows { get; }
+        ISite Site { get; set; }
+        string TableName { get; set; }
+        void AcceptChanges();
+        void BeginInit();
+        void BeginLoadData();
+        void Clear();
+        IDataTable Clone();
+        object Compute(string expression, string filter);
+        IDataTable Copy();
+        DataTableReader CreateDataReader();
+        void EndInit();
+        void EndLoadData();
+        IDataTable GetChanges();
+        IDataTable GetChanges(DataRowState rowStates);
+        DataRow[] GetErrors();
+        void GetObjectData(SerializationInfo info, StreamingContext context);
+        void ImportRow(DataRow row);
+        void Load(IDataReader reader);
+        void Load(IDataReader reader, LoadOption loadOption);
+        void Load(IDataReader reader, LoadOption loadOption, FillErrorEventHandler errorHandler);
+        DataRow LoadDataRow(object[] values, bool acceptChanges);
+        DataRow LoadDataRow(object[] values, LoadOption loadOption);
+        void Merge(DataTable table);
+        void Merge(DataTable table, bool preserveChanges);
+        void Merge(DataTable table, bool preserveChanges, MissingSchemaAction missingSchemaAction);
+        DataRow NewRow();
+        XmlReadMode ReadXml(Stream stream);
+        XmlReadMode ReadXml(TextReader reader);
+        XmlReadMode ReadXml(string fileName);
+        XmlReadMode ReadXml(XmlReader reader);
+        void ReadXmlSchema(Stream stream);
+        void ReadXmlSchema(TextReader reader);
+        void ReadXmlSchema(string fileName);
+        void ReadXmlSchema(XmlReader reader);
+        void RejectChanges();
+        void Reset();
+        DataRow[] Select();
+        DataRow[] Select(string filterExpression);
+        DataRow[] Select(string filterExpression, string sort);
+        DataRow[] Select(string filterExpression, string sort, DataViewRowState recordStates);
+        string ToString();
+        void WriteXml(Stream stream);
+        void WriteXml(Stream stream, bool writeHierarchy);
+        void WriteXml(TextWriter writer);
+        void WriteXml(TextWriter writer, bool writeHierarchy);
+        void WriteXml(XmlWriter writer);
+        void WriteXml(XmlWriter writer, bool writeHierarchy);
+        void WriteXml(string fileName);
+        void WriteXml(string fileName, bool writeHierarchy);
+        void WriteXml(Stream stream, XmlWriteMode mode);
+        void WriteXml(Stream stream, XmlWriteMode mode, bool writeHierarchy);
+        void WriteXml(TextWriter writer, XmlWriteMode mode);
+        void WriteXml(TextWriter writer, XmlWriteMode mode, bool writeHierarchy);
+        void WriteXml(XmlWriter writer, XmlWriteMode mode);
+        void WriteXml(XmlWriter writer, XmlWriteMode mode, bool writeHierarchy);
+        void WriteXml(string fileName, XmlWriteMode mode);
+        void WriteXml(string fileName, XmlWriteMode mode, bool writeHierarchy);
+        void WriteXmlSchema(Stream stream);
+        void WriteXmlSchema(Stream stream, bool writeHierarchy);
+        void WriteXmlSchema(TextWriter writer);
+        void WriteXmlSchema(TextWriter writer, bool writeHierarchy);
+    }
+}
