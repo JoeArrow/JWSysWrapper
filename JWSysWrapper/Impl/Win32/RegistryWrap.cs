1a66106a936030682d6152b51c78c98a724b614f
Joe Arrowood <jarrow@ctvea.net>
2022-01-22 16:20:47 -0500

    Added RegistryWrap

---
+++ JWSysWrapper/Impl/Win32/RegistryWrap.cs
@@ -0,0 +1,44 @@
+﻿#region © 2022 JoeWare.
+//
+// All rights reserved. Reproduction or transmission in whole or in part, in
+// any form or by any means, electronic, mechanical, or otherwise, is prohibited
+// without the prior written consent of the copyright owner.
+//
+#endregion
+
+using JWSysWrap.Interface.Win32;
+
+using Microsoft.Win32;
+
+namespace JWSysWrap.Impl.Win32
+{
+    // ----------------------------------------------------
+    /// <summary>
+    ///     RegistryWrap Description
+    /// </summary>
+
+    public class RegistryWrap : IRegistry
+    {
+        public RegistryKey CurrentUser => Registry.CurrentUser;
+
+        public RegistryKey LocalMachine => Registry.LocalMachine;
+
+        public RegistryKey ClassesRoot => Registry.ClassesRoot;
+
+        public RegistryKey Users => Registry.Users;
+
+        public RegistryKey PerformanceData => Registry.PerformanceData;
+
+        public RegistryKey CurrentConfig => Registry.CurrentConfig;
+
+        [System.Obsolete]
+        public RegistryKey DynData => Registry.DynData;
+
+        public object GetValue(string keyName, string valueName, object defaultValue) => Registry.GetValue(keyName, valueName, defaultValue);
+
+        public void SetValue(string keyName, string valueName, object value) => Registry.SetValue(keyName, valueName,  value);
+
+        public void SetValue(string keyName, string valueName, object value, RegistryValueKind valueKind) => 
+            Registry.SetValue(keyName, valueName, value, valueKind);
+    }
+}
---
+++ JWSysWrapper/Interface/Win32/IRegistry.cs
@@ -0,0 +1,27 @@
+﻿#region © 2022 JoeWare.
+//
+// All rights reserved. Reproduction or transmission in whole or in part, in
+// any form or by any means, electronic, mechanical, or otherwise, is prohibited
+// without the prior written consent of the copyright owner.
+//
+#endregion
+
+using Microsoft.Win32;
+
+namespace JWSysWrap.Interface.Win32
+{
+    public interface IRegistry
+    {
+        RegistryKey CurrentUser { get; }
+        RegistryKey LocalMachine { get; }
+        RegistryKey ClassesRoot { get; }
+        RegistryKey Users { get; }
+        RegistryKey PerformanceData { get; }
+        RegistryKey CurrentConfig { get; }
+        RegistryKey DynData { get; }
+
+        object GetValue(string keyName, string valueName, object defaultValue);
+        void SetValue(string keyName, string valueName, object value);
+        void SetValue(string keyName, string valueName, object value, RegistryValueKind valueKind);
+    }
+}
