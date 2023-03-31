3d5bc9e05ffd2dc22897ae7e27a15fd672eb477f
Joe Arrowood <jarrow@ctvea.net>
2023-03-31 06:12:20 -0400

    Copyright correction

--- JWSysWrapper/Impl/ActiveDirectory/DirectoryEntriesWrap.cs
+++ JWSysWrapper/Impl/ActiveDirectory/DirectoryEntriesWrap.cs
@@ -1,4 +1,4 @@
-#region © 2020 Aflac.
+#region © 2020 JoeWare.
 //
 // All rights reserved. Reproduction or transmission in whole or in part, in
 // any form or by any means, electronic, mechanical, or otherwise, is prohibited
